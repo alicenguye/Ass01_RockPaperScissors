38b548dca1a5bde99efef9638707b41a1e109669
Alice Nguyen <nguye4hn@mail.uc.edu>
2023-02-26 14:47:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String playerA = "";
+        String playerB = "";
+        String continueGame = "";
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            while (true) {
+
+                System.out.println("what's your move Player A: R, P or S?");
+                playerA = in.nextLine();
+                if (playerA.equals("R") || playerA.equals("P") || playerA.equals("S")) {
+                    break;
+                }
+                System.out.println("that is not a valid move.");
+            }
+
+
+            while (true) {
+
+                System.out.println("what's your move Player B: R, P or S?");
+                playerB = in.nextLine();
+                if (playerB.equals("R") || playerB.equals("P") || playerB.equals("S")) {
+                    break;
+                }
+                System.out.println("that is not a valid move.");
+            }
+
+            if (playerA.equals(playerB)) {
+                System.out.println("tie game!");
+            } else if (playerA.equals("R")) {
+                if (playerB.equals("P")) {
+                    System.out.println(" Paper covers Rock");
+                    System.out.println("Player B win");
+                }
+            } else if (playerA.equals("P")) {
+                if (playerB.equals("R")) {
+                    System.out.println(" Paper covers Rock");
+                    System.out.println("Player A win");
+                }
+            } if (playerA.equals("S")) {
+                if (playerB.equals("P")) {
+                    System.out.println("Scissors cut Paper");
+                    System.out.println("Player B win ");
+                }
+            }if (playerA.equals("P")) {
+                if (playerB.equals("S")) {
+                    System.out.println("Scissors cut Paper");
+                    System.out.println("Player B win ");
+                }
+            }
+            else if (playerA.equals("S")) {
+                if (playerB.equals("R")) {
+                    System.out.println("Rock break Scissors");
+                    System.out.println(" player B win ");
+                }
+            }
+                    if (playerA.equals("R")) {
+                    if (playerB.equals("S")) {
+                            System.out.println("Rock break Scissors");
+                            System.out.println("Player A win ");
+                        }
+                    }
+
+                    System.out.println(" Play again? Y/N");
+                    continueGame = in.nextLine();
+                    if (continueGame.equals("N")) {
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
