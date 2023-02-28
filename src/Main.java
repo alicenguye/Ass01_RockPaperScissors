6db905aa81e529d2806888f01cc57b08010855d4
Alice Nguyen <125220059+alicenguye@users.noreply.github.com>
2023-02-27 20:51:17 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //declaration of variables
         String playerA = "";
         String playerB = "";
         String continueGame = "";
-
+       // create new scanner
         Scanner in = new Scanner(System.in);
+        // start looping
         while (true) {
             while (true) {
 
                 System.out.println("what's your move Player A: R, P or S?");
                 playerA = in.nextLine();
                 if (playerA.equals("R") || playerA.equals("P") || playerA.equals("S")) {
-                    break;
+                    break;// only if it's matches uppercase R P or S
                 }
-                System.out.println("that is not a valid move.");
+                System.out.println("that is not a valid move.");// if they input something else
             }
 
 
@@ -24,11 +26,11 @@ public class Main {
                 System.out.println("what's your move Player B: R, P or S?");
                 playerB = in.nextLine();
                 if (playerB.equals("R") || playerB.equals("P") || playerB.equals("S")) {
-                    break;
+                    break;// only if it's matches uppercase R P or S
                 }
-                System.out.println("that is not a valid move.");
+                System.out.println("that is not a valid move.");// if they input something else
             }
-
+            // game start
             if (playerA.equals(playerB)) {
                 System.out.println("tie game!");
             } else if (playerA.equals("R")) {
@@ -38,37 +40,42 @@ public class Main {
                 }
             } else if (playerA.equals("P")) {
                 if (playerB.equals("R")) {
+                    //display output
                     System.out.println(" Paper covers Rock");
                     System.out.println("Player A win");
                 }
             } if (playerA.equals("S")) {
                 if (playerB.equals("P")) {
+                    //display output
                     System.out.println("Scissors cut Paper");
                     System.out.println("Player B win ");
                 }
             }if (playerA.equals("P")) {
                 if (playerB.equals("S")) {
+                    //display output
                     System.out.println("Scissors cut Paper");
                     System.out.println("Player B win ");
                 }
             }
             else if (playerA.equals("S")) {
                 if (playerB.equals("R")) {
+                    //display output
                     System.out.println("Rock break Scissors");
                     System.out.println(" player B win ");
                 }
             }
                     if (playerA.equals("R")) {
                     if (playerB.equals("S")) {
+                    //display output
                             System.out.println("Rock break Scissors");
                             System.out.println("Player A win ");
                         }
                     }
 
-                    System.out.println(" Play again? Y/N");
+                    System.out.println(" Play again? Y/N");//asking the user if they want to keep playing
                     continueGame = in.nextLine();
                     if (continueGame.equals("N")) {
-                        break;
+                        break;// if they enter N then the game end
                     }
                 }
             }
